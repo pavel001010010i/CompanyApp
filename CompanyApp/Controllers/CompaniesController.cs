4c318d18fc40e116e7fca4bc904047af14022e65
Варец Павел Анатольевич <varets@rit.va>
2022-08-31 15:04:09 +0300

    Added: Model binding API

--- CompanyApp/Controllers/CompaniesController.cs
+++ CompanyApp/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using CompanyApp.ModelBinders;
 using Contracts.Managers;
 using Entities.DTO;
 using Entities.Model;
@@ -67,7 +68,7 @@ namespace CompanyApp.Controllers
         }
 
         [HttpGet("collection/({ids})", Name = "CompanyCollection")]
-        public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+        public IActionResult GetCompanyCollection([ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
         {
             if (ids == null)
             {
